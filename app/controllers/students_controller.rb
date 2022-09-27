9c0dae035e7baa05828ee6693c8279c47e2a5f8a
Kimani <elvis.kimani@student.moringaschool.com>
2022-09-27 12:25:17 +0300

    commit tree ae8a365ec8ac0dee91760eb5846c7b1b61553f5a

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = 
+    if params[:name] 
+        Student.find_by_last_name(params[:name]) || Student.find_by_first_name(params[:name])  
+    else
+       Student.all
+    end  
+     
     render json: students
   end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
+  
 
 end
